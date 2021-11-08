36aa62d46e72db71b86fc1a04b1c27dd6eff963b
MarkoMae <MarkoM2e@Gmail.com>
2021-11-08 17:12:26 +0200

    React

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="my-text">HeiHei</div>
+      <div className="my-text">HeyHey</div>
+  <button className="main-button">Start</button>
+    </div>
+
+  );
+}
+
+export default App;
